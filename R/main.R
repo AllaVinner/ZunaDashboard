896b58a27abf4aa53c596986828b17df43a37e80
AllaVinner <joel.w.ottosson@gmail.com>
2022-10-08 08:35:39 +0200

    Initial working working.

---
+++ R/main.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(shinydashboard)
+source('R/salesGenerator.R')
+source('R/sells_plots.R')
+
+
+
+product_ui <- function(id) {
+  message('product ui running')
+  tagList(
+    tableOutput(NS(id, 'table')),
+    plotlyOutput(NS(id, 'plot'))
+  )
+} 
+
+ui <- dashboardPage(
+  dashboardHeader(title = 'Sells Detective'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Product Story", tabName = "product", icon = icon("arrow-trend-up", verify_fa = FALSE),
+               selected = TRUE)
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = 'product', product_ui('product'))
+    )
+  )
+)
+
+product_server <- function(id, db) {
+  moduleServer(id, function(input, output, session) {
+    message('In product server')
+    output$table <- renderTable(head(db$products) %>% discard(~typeof(.x) == 'list'))
+    output$plot <- renderPlotly(sell_story2(db$sells))
+    
+  })
+}
+
+server <- function(input, output) {
+  sells <- list(
+    shirts = generate_beta_sells(100, as.Date('2022-01-01'), as.Date('2022-06-30'), 2, 5),
+    pants = generate_beta_sells(70, as.Date('2022-01-01'), as.Date('2022-06-30'), 3, 4) 
+  )
+  
+  
+  products <- tibble(
+    name = c('Shirt', 'Pants'),
+    type = c('Shirt', 'Pants'),
+    price = c(590, 750)
+  )
+  sells = bind_rows(
+    tibble(name = 'Shirt', sold = sells$shirts),
+    tibble(name = 'Pants', sold = sells$pants)
+  )
+  
+  db <- list(
+    products = products,
+    sells = sells
+  )
+  
+  db$products <- merge(db$products, db$sells %>% group_by(name) %>% summarize(len = n()))
+  
+  product_server('product', db)
+}
+
+
+
+#f <- sell_story(products, 'shirt')
+
+shinyApp(ui, server)
+
+
--- R/sells_plots.R
+++ R/sells_plots.R
@@ -1,7 +1,8 @@
 library('dplyr')
 library('plotly')
 library('tibble')
-library(ggplot2)  
+library(ggplot2) 
+library('purrr')
 source('R/salesGenerator.R')
 
 sell_story <- function(sell_tbl, product) {
@@ -25,3 +26,44 @@ sell_story <- function(sell_tbl, product) {
              zerolinewidth = 2,   
              gridcolor = 'ffff')) 
 }
+
+
+sell_story_single <- function(fig, column_name) {
+  fig +
+    geom_density(
+        mapping = aes(x = .data[[column_name]], color = 'density'), 
+        color = '#67B7D1'
+    ) +  
+    geom_rug() + 
+    ylab("") + 
+    xlab("")  + 
+    scale_color_manual(values = c('density' = '#67B7D1'))
+}
+
+
+sell_story2 <- function(sell_tbl) {
+  gg <- ggplot(sell_tbl) + 
+    geom_density(
+      mapping = aes(x = sold, color = name)
+    ) +  
+    geom_rug(
+      mapping = aes(x = sold, color = name)
+    ) + 
+    ylab("") + 
+    xlab("")
+
+  ggplotly(gg)%>% 
+    layout(plot_bgcolor='#e5ecf6',
+           xaxis = list(   
+             title='Time', 
+             zerolinecolor = '#ffff', 
+             zerolinewidth = 2,   
+             gridcolor = 'ffff'),
+           yaxis = list(   
+             title='# Sells per year',
+             zerolinecolor = '#ffff', 
+             zerolinewidth = 2,   
+             gridcolor = 'ffff')) 
+}
+
+
